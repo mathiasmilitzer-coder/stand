6d7bb814637b2c458d3ad14727085d119fb0ecbe
mathiasmilitzer-coder <mathiasmilitzer@googlemail.com>
2025-11-16 09:37:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# Erstellt eine Flask-Instanz
 app = Flask(__name__)
 
-# Definiert die Route für die Startseite ("/")
 @app.route('/')
-def hello_world():
-    # Gibt den Text zurück, der im Browser angezeigt werden soll
-    return '<h1>Hello, World!</h1><p>Das ist Ihre erste Flask-Seite.</p>'
-
-# Startet den Entwicklungsserver, wenn die Datei direkt ausgeführt wird
-if __name__ == '__main__':
-    app.run(debug=True)
+def home():
+    # Flask sucht automatisch im 'templates'-Ordner
+    return render_template('index.html')
